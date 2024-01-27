0172eb9c6e0c4412e6e101939ac4f26ae363ef32
Deepthy <deepthydeepanjaly@gmail.com>
2024-01-27 12:11:24 +0530

    randomized email and fixed assertion messages

--- src/test/java/UserSignupTest.java
+++ src/test/java/UserSignupTest.java
@@ -2,14 +2,19 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.UUID;
+
+import java.util.UUID;
 
 public class UserSignupTest extends BaseAPITest{
-
-    public void successfulRegistration(){
+@Test
+public void successfulRegistration(){
         //Resource end ponit -> /api/auth/signup
         String signupEndpointResource = "/api/auth/signup";
-        String email =" test @gmail.com";
-        String password = "12345678990";
+        //String email ="test_6726227dee17277@gmail.com";
+        String email = "test" + UUID.randomUUID().toString() + "@gmail.com";
+        String password = "123456711";
         //Request body
         String signupRequestBody = String.format("{\"email\": \"%s\", \"password\": \"%s\"}", email, password);
 
@@ -24,13 +29,10 @@ public class UserSignupTest extends BaseAPITest{
         String role = response.jsonPath().get("data.user.role");
         String accessToken = response.jsonPath().get("data.session.access_token");
 
-        Assert.assertEquals(statusCode, 201, "Failed due to wrong status code");
-        Assert.assertEquals(authenticatedEmail, email, "Failed due to incorrect email");
-        Assert.assertEquals(role, "authenticated", "Failed dto validate role to be authenticated");
-        Assert.assertNotNull(accessToken, "Failed as access token is null");
-
-
-
+        Assert.assertEquals(statusCode, 201, "status code shpul be 201");
+        Assert.assertEquals(authenticatedEmail, email, "email is not matching entered email");
+        Assert.assertEquals(role, "authenticated", "role is not authenticated");
+        Assert.assertNotNull(accessToken, "access token is not null");
 
 
     }
